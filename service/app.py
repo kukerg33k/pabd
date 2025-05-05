4ccf3d4f36b18077136db5d0c51642e758dd2689
kukerg33k <kukerlevon@mail.ru>
2025-05-05 21:55:46 +0300

    Этап 3

--- service/app.py
+++ service/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request 
 from logging.config import dictConfig
+import joblib
 
 dictConfig(
     {
@@ -27,6 +28,14 @@ dictConfig(
 
 app = Flask(__name__)
 
+model = None
+try:
+    model = joblib.load('models\linear_regression_model.pkl')
+    app.logger.info("Model loaded successfully")
+except Exception as e:
+    app.logger.error(f"Error loading model: {str(e)}")
+
+
 # Маршрут для отображения формы
 @app.route('/')
 def index():
@@ -38,10 +47,31 @@ def process_numbers():
     # Здесь можно добавить обработку полученных чисел
     # Для примера просто возвращаем их обратно
     data = request.get_json()
-    
     app.logger.info(f'Requst data: {data}')
-    
-    return {'status': 'success', 'data': 'Числа успешно обработаны'}
+        
+    try:
+        area = [[float(data['area'])]]
+        
+        if model:
+            predicted_price = model.predict(area)[0]
+            return {
+                'status': 'success',
+                'predicted_price': predicted_price
+            }
+        else:
+            return {
+                'status': 'error',
+                'message': 'Model not loaded'
+            }, 500
+    except Exception as e:
+        app.logger.error(f"Prediction error: {str(e)}")
+        return {
+            'status': 'error',
+            'message': str(e)
+        }, 500
+
+
+    # return {'status': 'success', 'data': 'Числа успешно обработаны'}
 
 if __name__ == '__main__':
     app.run(debug=True)
