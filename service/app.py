1a114ae7b0e8bf17e5530c234b2c99c6d9c8ea38
kukerg33k <kukerlevon@mail.ru>
2025-05-05 19:48:32 +0300

    first try

---
+++ service/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request 
+from logging.config import dictConfig
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default",
+            },         
+            "file": {
+                "class": "logging.FileHandler",
+                "filename": "service/flask.log",
+                "formatter": "default",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+    }
+)
+
+app = Flask(__name__)
+
+# Маршрут для отображения формы
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Маршрут для обработки данных формы
+@app.route('/api/numbers', methods=['POST'])
+def process_numbers():
+    # Здесь можно добавить обработку полученных чисел
+    # Для примера просто возвращаем их обратно
+    data = request.get_json()
+    
+    app.logger.info(f'Requst data: {data}')
+    
+    return {'status': 'success', 'data': 'Числа успешно обработаны'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
