e02a0bdcc74aec9d59ffbc40533347c2030bfaa9
kukerg33k <kukerlevon@mail.ru>
2025-05-12 21:34:16 +0300

    Этап 4 ver 2

--- service/pipeline.py
+++ service/pipeline.py
@@ -111,11 +111,6 @@ def load_and_prepare_data(processed_path, train_size=TRAIN_SIZE):
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, train_size=train_size, random_state=42)
 
-        # Feature scaling
-        scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
-
         return X_train, X_test, y_train, y_test
 
     except Exception as e:
